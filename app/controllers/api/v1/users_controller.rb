dbc1acddacb5fd0598a3ec8895656fbcd8e126e0
github username <github email address>
2020-03-19 13:51:30 -0700

    Added namespacing to the users controller.

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -1,26 +1,26 @@
 class Api::V1::UsersController < ApplicationController
   before_action :set_user, only: [:show, :edit, :destroy]
 
-  def index 
-    @users = User.all 
+  def index
+    @users = User.all
     render json: @users
-  end 
+  end
 
-  def create 
+  def create
     @user = User.new(user_params)
-    if @user.save 
-      render json: @user, status: :created, location: @user 
-    else 
+    if @user.save
+      render json: @user, status: :created, location: @user
+    else
       render json: @user.errors, status: :unprocessable_entity
-    end 
-  end 
+    end
+  end
 
   private
-  
-    def set_user 
+
+    def set_user
       @user = User.find(params[:id])
-    end 
-    def user_params 
+    end
+    def user_params
       params.require(:user).permit(:username, :email, :password)
-    end 
+    end
 end
